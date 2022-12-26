970cb89c4ca5e7c22eb00340c7d1bd69bdca119b
Marious <nmarious.asilo@gmail.com>
2022-12-26 03:23:52 -0500

    Update password manager to save json file instead of txt and add search function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tkinter as tk
 import pyperclip
+import json
+import tkinter as tk
 from tkinter import END
 from tkinter import messagebox
 
@@ -26,37 +27,46 @@ def pw_generate():
     entry_password.insert(0, pw)
 
 
-# Register Password to txt
-def pw_registration(**kwargs):
-    reg_website = kwargs['website']
-    reg_username = kwargs['username']
-    reg_password = kwargs['password']
-    with open(file='password.txt', mode='a') as file:
-        file.write(f'{reg_website} | {reg_username} | {reg_password}\n')
-
-
-# Perform function by clicking 'Yes' button
-def pw_yes_button():
-    add_website = entry_website.get()
-    add_username = entry_username.get()
-    add_password = entry_password.get()
-    pw_registration(website=add_website, username=add_username, password=add_password)
-    entry_password.delete(0, END)
-    entry_website.delete(0, END)
-
-
 # Create confirmation pop-up window
 def pop_up():
-    pop_up_website = entry_website.get()
-    pop_up_username = entry_username.get()
-    pop_up_password = entry_password.get()
-    message = f'Email: {pop_up_username}\nPassword: {pop_up_password}\nDo you wish to proceed?'
-    if pop_up_password == '' or pop_up_website == '' or pop_up_username == '':
+    website = entry_website.get()
+    username = entry_username.get()
+    password = entry_password.get()
+    new_data = {
+        website:
+            {'email': username,
+             'password': password}
+    }
+    if password == '' or website == '' or username == '':
         tk.messagebox.showerror(title='Oops', message="Please don't leave any fields empty.")
     else:
-        res = tk.messagebox.askokcancel(title=pop_up_website, message=message)
-        if res:
-            pw_yes_button()
+        try:
+            with open(file='password.json') as file:
+                data = json.load(file)
+                data.update(new_data)
+        except:
+            with open(file='password.json', mode='w') as file:
+                json.dump(json.loads('{}'), file, indent=4)
+            with open(file='password.json') as file:
+                data = json.load(file)
+                data.update(new_data)
+        with open(file='password.json', mode='w') as file:
+            json.dump(data, file, indent=4)
+            entry_password.delete(0, END)
+            entry_website.delete(0, END)
+
+
+def search():
+    with open(file='password.json') as file:
+        data = json.load(file)
+    try:
+        search_website = data[entry_website.get()]
+    except KeyError:
+        messagebox.showerror(title='Details Not Found', message=f'There is no {entry_website.get()} on file')
+    else:
+        search_username = search_website['email']
+        search_password = search_website['password']
+        messagebox.showinfo(title=entry_website.get(), message=f'Email: {search_username}\nPassword: {search_password}')
 
 
 # Create widgets for buttons, labels and entry
@@ -64,11 +74,12 @@ tk.Label(text='Website: ', master=root).grid(row=1, column=0)
 tk.Label(text='Email/Username: ', master=root).grid(row=2, column=0)
 tk.Label(text='Password: ', master=root).grid(row=3, column=0)
 
+tk.Button(text='Search', master=root, width=14, command=search).grid(row=1, column=2)
 tk.Button(text='Generate Password', master=root, command=pw_generate).grid(row=3, column=2)
 tk.Button(text='Add', width=42, master=root, command=pop_up).grid(row=4, column=1, columnspan=2, pady=2)
 
-entry_website = tk.Entry(width=50, master=root)
-entry_website.grid(row=1, column=1, columnspan=2, sticky='w', pady=2)
+entry_website = tk.Entry(width=30, master=root)
+entry_website.grid(row=1, column=1, columnspan=1, sticky='w', pady=2)
 entry_website.focus()
 entry_username = tk.Entry(width=50, master=root)
 entry_username.grid(row=2, column=1, columnspan=2, sticky='w', pady=2)
